2da7499dfc684d51620301026947089c6a46e638
sarrahman <sarrahman.dev@gmail.com>
2022-08-31 21:01:25 +0600

    edit sekali lagi

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-    defaultExtractor: (content) => {
-      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-      const innerMatches =
-        content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-      return broadMatches.concat(innerMatches);
-    },
-  },
-];
 module.exports = {
   plugins: [
-    "tailwindcss",
-    process.env.NODE_ENV === "production" ? purgecss : undefined,
-    "postcss-preset-env",
+    'tailwindcss',
+    'postcss-preset-env',
   ],
-};
+}
