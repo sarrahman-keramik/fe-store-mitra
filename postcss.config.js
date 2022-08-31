086f62b233bc4ba263f5ef3d2ea74fe472a40aa5
sarrahman <sarrahman.dev@gmail.com>
2022-08-31 20:46:23 +0600

    :bug: edit post css

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,25 @@
-// mungkin akan muncul warning di terminal karena return undefined saat tidak production
-
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    // Specify the paths to all of the template files
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './src/components/**/*.{js,jsx,ts,tsx}',
+    ],
+    // This is the function used to extract class names from the templates
+    defaultExtractor: (content) => {
+      // Capture as liberally as possible, including things like `h-(screen-1.5)`
+      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
+      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
+      return broadMatches.concat(innerMatches);
+    },
+  },
+];
 module.exports = {
   plugins: [
-    "tailwindcss",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              "./pages/**/*.{ts,tsx}",
-              ".src/components/**/*.{ts,tsx}",
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
-    "postcss-preset-env",
+    'tailwindcss',
+    process.env.NODE_ENV === 'production' ? purgecss : undefined,
+    'postcss-preset-env',
   ],
 };
