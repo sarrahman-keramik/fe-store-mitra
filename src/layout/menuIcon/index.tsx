8ed4b94a7d28e0804933f497799d8cd40656d24b
sarrahman <sarrahman.dev@gmail.com>
2022-11-07 07:48:16 +0800

    redesign menu icon

--- src/layout/menuIcon/index.tsx
+++ src/layout/menuIcon/index.tsx
@@ -4,7 +4,10 @@ import tiles from "../../../public/tiles.png";
 import beam from "../../../public/beam.png";
 import roof from "../../../public/roof.png";
 import Image from "next/image";
-import menu from "../../../public/menu.png";
+// import menu from "../../../public/menu.png";
+import wood from "../../../public/wood.png";
+import cement from "../../../public/cement.png";
+import water_tank from "../../../public/water-tank.png";
 import { SectionLayout } from "../../template";
 
 export default function MenuIcon() {
@@ -12,16 +15,16 @@ export default function MenuIcon() {
     <SectionLayout
       title="Kategori"
       main={
-        <div className="grid grid-cols-3 gap-5">
+        <div className="grid grid-cols-4 gap-3">
           <Menu image={beam} title="Baja Ringan" />
           <Menu image={roof} title="Atap" />
           <Menu image={tiles} title="Granit" />
           <Menu image={pipe} title="Pipa" />
           <Menu image={iron} title="Besi" />
-          <Menu image={menu} title="Lainnya" />
-          {/* <Menu image={wood} title="Plywood" /> */}
-          {/* <Menu image={cement} title="Semen" />
-          <Menu image={water_tank} title="Tandon" /> */}
+          {/* <Menu image={menu} title="Lainnya" /> */}
+          <Menu image={wood} title="Plywood" />
+          <Menu image={cement} title="Semen" />
+          <Menu image={water_tank} title="Tandon" />
         </div>
       }
     />
@@ -37,9 +40,9 @@ function Menu(props: { title: string; image: any }) {
           <Image src={props.image} alt="pipe" width={100} height={100} />
         </div>
         <div className="md:hidden">
-          <Image src={props.image} alt="pipe" width={75} height={75} />
+          <Image src={props.image} alt="pipe" width={50} height={50} />
         </div>
-        <h3 className="text-center text-sm md:text-base font-bold text-teal-800">
+        <h3 className="text-center text-xs md:text-base font-bold text-teal-800">
           {props.title}
         </h3>
       </div>
