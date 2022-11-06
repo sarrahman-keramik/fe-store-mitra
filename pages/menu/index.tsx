079ef337a7030e6072434da137f1ba1531ff9877
sarrahman <sarrahman.dev@gmail.com>
2022-11-07 07:29:31 +0800

    add images

---
+++ pages/menu/index.tsx
@@ -0,0 +1,11 @@
+import { StoreLayout } from "../../src/template";
+
+export default function Menu(){
+    return(
+       <StoreLayout title="Menu" main={
+        <div>
+            list menu
+        </div>
+       } />
+    )
+}
--- src/layout/menuIcon/index.tsx
+++ src/layout/menuIcon/index.tsx
@@ -3,22 +3,25 @@ import iron from "../../../public/iron-bar.png";
 import tiles from "../../../public/tiles.png";
 import beam from "../../../public/beam.png";
 import roof from "../../../public/roof.png";
-import wood from "../../../public/wood.png";
 import Image from "next/image";
+import menu from "../../../public/menu.png";
 import { SectionLayout } from "../../template";
 
 export default function MenuIcon() {
   return (
     <SectionLayout
-    title="Kategori"
+      title="Kategori"
       main={
         <div className="grid grid-cols-3 gap-5">
           <Menu image={beam} title="Baja Ringan" />
           <Menu image={roof} title="Atap" />
-          <Menu image={tiles} title="Granit & Keramik" />
-          <Menu image={pipe} title="Pipa Pvc" />
-          <Menu image={iron} title="Besi Beton" />
-          <Menu image={wood} title="Plywood" />
+          <Menu image={tiles} title="Granit" />
+          <Menu image={pipe} title="Pipa" />
+          <Menu image={iron} title="Besi" />
+          <Menu image={menu} title="Lainnya" />
+          {/* <Menu image={wood} title="Plywood" /> */}
+          {/* <Menu image={cement} title="Semen" />
+          <Menu image={water_tank} title="Tandon" /> */}
         </div>
       }
     />
@@ -36,7 +39,9 @@ function Menu(props: { title: string; image: any }) {
         <div className="md:hidden">
           <Image src={props.image} alt="pipe" width={75} height={75} />
         </div>
-        <h3 className="text-center font-bold text-teal-800">{props.title}</h3>
+        <h3 className="text-center text-sm md:text-base font-bold text-teal-800">
+          {props.title}
+        </h3>
       </div>
     </div>
   );
